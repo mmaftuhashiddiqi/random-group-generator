ec2ad48437f7e9383996a5f7a47229ef3ba4cb43
mmaftuhashiddiqi <mmaftuh.2021@student.uny.ac.id>
2023-03-04 21:08:07 +0700

    menyelesaikan project random group generator

---
+++ script.js
@@ -0,0 +1,74 @@
+const inputName = document.getElementById('input-name');
+const groupCount2 = document.querySelector('.group-count-2');
+const groupCount3 = document.querySelector('.group-count-3');
+const groupCount4 = document.querySelector('.group-count-4');
+const groupCount5 = document.querySelector('.group-count-5');
+const groupCount6 = document.querySelector('.group-count-6');
+const groupCount7 = document.querySelector('.group-count-7');
+const groupCountCustom = document.querySelector('.group-count-custom');
+const generateButton = document.querySelector('.generate');
+const groupResults = document.querySelector('.group-results');
+
+
+let groupCount;
+groupCount2.addEventListener('click', function() {
+    groupCount = 2;
+});
+groupCount3.addEventListener('click', function() {
+    groupCount = 3;
+});
+groupCount4.addEventListener('click', function() {
+    groupCount = 4;
+});
+groupCount5.addEventListener('click', function() {
+    groupCount = 5;
+});
+groupCount6.addEventListener('click', function() {
+    groupCount = 6;
+});
+groupCount7.addEventListener('click', function() {
+    groupCount = 7;
+});
+groupCountCustom.addEventListener('input', function() {
+    groupCount = parseInt(groupCountCustom.value);
+});
+
+
+generateButton.addEventListener('click', function() {
+    groupResults.innerHTML = '';
+
+    const members = inputName.value.split('\n');
+    const randomMembers = members.sort(() => Math.random() - 0.5);
+
+    if (groupCount > randomMembers.length) {
+        groupCount = randomMembers.length;
+    }
+
+    const part = (a, n) => {
+        const b = Math.max(a.length / n);
+        return [...Array(n)].map((_, i) => a.slice(i * b, (i + 1) * b));
+    };
+    const resultMembers = part(randomMembers, groupCount);
+
+    for (let i = 0; i < groupCount; i++) {
+        groupResults.insertAdjacentHTML(`beforeend`, `
+        <div class="group-container">
+            <table class="group">
+                <thead>
+                    <tr>
+                        <th>Group ${i+1}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            ${
+                                resultMembers[i].map(member => `<li>${member}</li>`).join('')
+                            }
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>`);
+    }
+});
